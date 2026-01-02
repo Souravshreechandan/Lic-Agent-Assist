583173435f3c131f175b0d862ace0102d14d1c65
Souravshreechandan <154435201+Souravshreechandan@users.noreply.github.com>
2026-01-02 10:17:32 +0530

    added versel.json

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,16 +1,12 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.PROD
-    ? "/api"
-    : "http://localhost:5000/api"
+  baseURL: import.meta.env.VITE_API_URL
 });
 
 api.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
-  if (token) {
-    req.headers.Authorization = `Bearer ${token}`;
-  }
+  if (token) req.headers.authorization = `Bearer ${token}`;
   return req;
 });
 
